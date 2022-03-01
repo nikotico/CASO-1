cd1a71fc4ced843463e8683265b6d68d57f132bb
NikoTico <52984591+nikotico@users.noreply.github.com>
2022-03-01 14:51:35 -0600

    .net Qr1

--- net/qr1/Program.cs
+++ net/qr1/Program.cs
@@ -11,52 +11,179 @@ dotnet run
 
 */
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Data.SqlClient ;
-using System.Data;  
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.Data.SqlClient;
+
 
 //"Server=***;Initial Catalog=***;Persist Security Info=False;User  ID=***;Password=***;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;";
-string connString =@"Server=NIKOF;Initial Catalog=aseni;Persist Security Info=False;User ID=sa;Password=admin;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;";
 
-try
+function.Main();
+public static class Globals
 {
-    using (SqlConnection conn = new SqlConnection(connString))
-    {
-        //retrieve the SQL Server instance version
-        string query = @"SELECT * FROM qr3Min";
-
-        SqlCommand cmd = new SqlCommand(query, conn);
-
-        //open connection
-        conn.Open();
-
-        //execute the SQLCommand
-        SqlDataReader dr = cmd.ExecuteReader();
-
-        //check if there are records
-        if (dr.HasRows)
-        {
-            while (dr.Read())
-            {
-                //display retrieved record (first column only/string value)
-                String partido = ( dr.GetString(0).Trim()  );
-                String accion = ( dr.GetString(1).Trim() );
-                String canton = ( dr.GetString(2).Trim() );
-                Console.WriteLine(partido+" \t"+accion+" \t"+canton+" "+dr.GetInt32(3));
-            }
-        }
-        else
-        {
-            Console.WriteLine("No data found.");
-        }
-        dr.Close();
-    }
+    public static string connString = @"Server=NIKOF;Initial Catalog=aseni;Persist Security Info=False;User ID=sa;Password=admin;MultipleActiveResultSets=False;Encrypt=False;TrustServerCertificate=False;Connection Timeout=30;";
+    public static SqlConnection conn = null;
+    public static SqlCommand cmd = null;
 }
-catch (Exception ex)
+/*
+Explicacion al final
+El tiempo promedio es de 3080 ms, pero tengo que tener en cuenta que hago sleeps de 300 milisegundos
+Entonces restando ese tiempo tengo que la duracion es de 0.80 milisegundos
+*/
+public static class function
 {
-    //display error message
-    Console.WriteLine("Exception: " + ex.Message);
+    public static void Main()
+    {
+
+        Thread t0 = new Thread(new ThreadStart(consulta));
+        Thread t1 = new Thread(new ThreadStart(consulta));
+        Thread t2 = new Thread(new ThreadStart(consulta));
+        Thread t3 = new Thread(new ThreadStart(consulta));
+        Thread t4 = new Thread(new ThreadStart(consulta));
+        Thread t5 = new Thread(new ThreadStart(consulta));
+        Thread t6 = new Thread(new ThreadStart(consulta));
+        Thread t7 = new Thread(new ThreadStart(consulta));
+        Thread t8 = new Thread(new ThreadStart(consulta));
+        Thread t9 = new Thread(new ThreadStart(consulta));
+        Thread.Sleep(2000);//Tiempo para el procesador para ejecutar todo
+
+        Stopwatch timeMeasure = new Stopwatch();
+        timeMeasure.Start();
+
+        t0.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t1.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t2.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t3.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t4.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t5.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t6.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t7.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t8.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        t9.Start();
+        Thread.Sleep(300);//Sin esto las conecciones se pierden y se realiza la consulta
+        timeMeasure.Stop();
+        Console.WriteLine($"Tiempo: {timeMeasure.Elapsed.TotalMilliseconds} ms");
+
+
+    }
+
+    public static void consulta()
+    {
+        Random rnd = new Random();
+        List<string> cantones = new List<string>() { "Naranjo", "Alajuela", "Grecia", "Atenas" };
+        string canton = cantones[rnd.Next(0, 3)];
+        Console.WriteLine(canton + "\n");
+        try
+        {
+            using (Globals.conn = new SqlConnection(Globals.connString))//Se habre la connection)
+            {
+                //retrieve the SQL Server instance version
+                string query = @"exec qr1 @canton = " + canton;
+
+                Globals.cmd = new SqlCommand(query, Globals.conn);
+
+                //open connection
+                Globals.conn.Open();
+
+                //execute the SQLCommand
+                SqlDataReader dr = Globals.cmd.ExecuteReader();
+
+                //check if there are records
+                if (dr.HasRows)
+                {
+                    while (dr.Read())
+                    {
+                        //display retrieved record
+                        String partido = (dr.GetString(0).Trim());
+                        Console.WriteLine(partido + " \t" + dr.GetInt32(1));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No data found.");
+                }
+                dr.Close();
+            }
+            Console.WriteLine("\n");
+        }
+        catch (Exception ex)
+        {
+            //display error message
+            Console.WriteLine("ERROR: ");
+            Console.WriteLine("Exception: " + ex.Message);
+        }
+    }
+    /*
+    C:\Users\fgm_o\Documentos\TEC\5 Semestre\Bases ll\Caso #1\net\qr1>dotnet run
+
+    Tiempo: 3077,4217 ms
+    Naranjo
+
+    PAC     2
+    PLN     2
+    PSD     1
+    PUSC    2
+
+
+    Grecia
+
+    PSD     1
+
+
+    Alajuela
+
+    PAC     1
+    PLN     1
+    PSD     2
+
+
+    Grecia
+
+    PSD     1
+
+
+    Grecia
+
+    PSD     1
+
+
+    Alajuela
+
+    PAC     1
+    PLN     1
+    PSD     2
+
+
+    Grecia
+
+    PSD     1
+
+
+    Grecia
+
+    PSD     1
+
+
+    Grecia
+
+    PSD     1
+
+
+    Naranjo
+
+    PAC     2
+    PLN     2
+    PSD     1
+    PUSC    2
+
+    */
 }
